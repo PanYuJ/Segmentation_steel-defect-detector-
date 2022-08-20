fba9a733bf46a649112f4c28ca38ed8aff497f1f
PanYuJ <109787213+PanYuJ@users.noreply.github.com>
2022-08-20 22:58:51 +0800

    Update Dataloader.py

--- Dataloader.py
+++ Dataloader.py
@@ -5,9 +5,14 @@ import sklearn
 
 # https://www.kaggle.com/competitions/severstal-steel-defect-detection
 
-def loader(file_path):
-# Extract zip file including image and label after download from Kaggle  
-  file_path = './kaggle'
+def loader(file_path, zip_path):
+ 
+"""
+Arg:
+  file_path: The path of repository that storing Raw datas.
+  zip_path: The path of zip file.
+"""
+  # Extract zip file including image and label after download from Kaggle  
   if not os.path.isdir(file_path):
     os.makedirs(file_path)
 
@@ -17,33 +22,24 @@ def loader(file_path):
 
   # Bulid data list in pandas Dataframe
   ## Colume of list: ImageId_ClassId, ClassId, EncodedPixels
-  img_list = list(os.listdir(os.path.join('file_path', 'train_images')))
-  img_list = sorted(img_list)
-  train_df = pd.DataFrame(columns=['ImageId_ClassId', 'EncodedPixels'])
-  for i in tqdm(img_list):
-      for j in range(4):
-          tmp_se = pd.Series( [i+'_{}'.format(j+1),np.nan], index=train_df.columns )
-          train_df = train_df.append(tmp_se, ignore_index=True )
+  df = pd.read_csv("./kaggle/train.csv")
 
+  df['ClassId'] = df['ClassId'].astype(int)
+  df.sort_values(by=['ImageId'])
+  df_tmp = df.pivot(index='ImageId', columns='ClassId', values='EncodedPixels')
+  
   # Change colume to ImageId, e1, e2, e3, e4, count
-  train_df['ImageId'] = train_df['ImageId_ClassId'].map(lambda x: x.split('.')[0]+'.jpg')
-  train_df2 = pd.DataFrame({'ImageId':train_df['ImageId'][::4]})
-  train_df2['e1'] = train_df['EncodedPixels'][::4].values
-  train_df2['e2'] = train_df['EncodedPixels'][1::4].values
-  train_df2['e3'] = train_df['EncodedPixels'][2::4].values
-  train_df2['e4'] = train_df['EncodedPixels'][3::4].values
+  train_df2 = pd.DataFrame(df_tmp.index)
+  train_df2['e1'] = df_tmp[:].iloc[:,0].values
+  train_df2['e2'] = df_tmp[:].iloc[:,1].values
+  train_df2['e3'] = df_tmp[:].iloc[:,2].values
+  train_df2['e4'] = df_tmp[:].iloc[:,3].values
   train_df2.reset_index(inplace=True,drop=True)
   train_df2.fillna('',inplace=True); 
   train_df2['count'] = np.sum(train_df2.iloc[:,1:]!='',axis=1).values
 
-  # Remove 'Nan' in 'EncodedPixels' colume
-  indexNames = train_df2[ train_df2['count'] == 0].index
-  train_df2_drop = train_df2.copy()
-  train_df2_drop.drop(indexNames , inplace=True)
-  train_df2_drop.reset_index(inplace=True, drop=True)
-
   # Shuffle list
-  train_df2_shuffle = sklearn.utils.shuffle(train_df2_drop, random_state=2000)
+  train_df2_shuffle = sklearn.utils.shuffle(train_df2, random_state=2000)
   train_df2_shuffle.reset_index(inplace=True, drop=True)
   
   return train_df2_shuffle
